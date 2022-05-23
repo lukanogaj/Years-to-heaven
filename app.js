ad67ce821cf196d0c3cbd96339584faa5a3bf5fe
Lukasz Nogaj <lukasznogaj@Lukaszs-Mac-mini.local>
2022-05-23 17:03:46 +0100

    frustration

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 /*Ja bym zrobil funkcje, ktora odlicza czas do retirementu, taki countdown clock i pokazal to na stronie z inputem daty urodzenia, czyli masz input, button i countdown,
 
 Wpisujesz swoja date urodzenia (wliczajac miesiac i dzien) klikasz 'start countdown' i pokazuje Ci sie odliczanie do retirementu, ktore odlicza lata, dni, godziny, minuty, sekundy w formacie 10:234:17:54:22 */
-
-const retirement = 65;
+const retireAge = 65;
 
 // Select the elements
 const btnBirthOfYear = document.querySelector('#btn-submit-dateOfBirth');
 const btnTodayDate = document.querySelector('#btn-submit-todayDate');
-
+// Select the value from the inputs
+const todayDateValue = document.querySelector('#todayDate');
+const birthDateValue = document.querySelector('#birthDate');
 //  Add functionality to the buttons
 // Todays date button
 btnTodayDate.addEventListener('click', () => {
@@ -27,7 +28,26 @@ btnBirthOfYear.addEventListener('click', () => {
   } else {
     btnBirthOfYear.innerText = 'Calculate years to retire';
   }
+  // const yearsToRetire = function (currentDate, birthYear, retireAge) {
+  //   const age = todayDateValue.value - birthDateValue.value;
+  //   return retireAge - age;
+  // };
 });
 
+// Functions to calculate years to retire
+
+const yearsToRetire = function (currentDate, birthDateValue, retireAge) {
+  const age = todayDateValue.value - birthDateValue.value;
+  console.log(age);
+};
+
+console.log(
+  yearsToRetire(todayDateValue.value, birthDateValue.value) - retireAge
+);
+
+// console.log(yearsToRetire());
+
+// Checking if the value is submited and create the function to calc
+
 // Checking and the debbuing
-console.log(btnBirthOfYear, btnTodayDate);
+console.log(todayDateValue.value, birthDateValue.value);
